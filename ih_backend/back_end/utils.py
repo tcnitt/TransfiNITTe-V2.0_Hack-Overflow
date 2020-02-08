1556499a86731787bb65d21a92ee413c787bc8e8
Karthik <karthikswaroopz@gmail.com>
2020-02-08 11:43:36 +0530

    Dunno which commit no. xD

---
+++ ih_backend/back_end/utils.py
@@ -0,0 +1,11 @@
+# Checks the model, if the data isn't already present, send an sms to the given number
+def hello():
+    import requests
+    url = "https://www.fast2sms.com/dev/bulk"
+    payload = "sender_id=FSTSMS&message=Siiii&language=english&route=p&numbers=9962017896"
+    headers = {'authorization': "gjnoaMcX65G80yl3zeKxD7PibwrHWBZIVkY9LtJ4dEup2qFsNSpGaFX0yMRsc6HjhPeJzKwNEqCgrBOu",'Content-Type': "application/x-www-form-urlencoded",'Cache-Control': "no-cache",}
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+
+hello()
+
